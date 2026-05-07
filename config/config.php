8e800925b2eafbd647280d5bf923e5ec6b4dc98a
siladennisn <siladennis1256@gmail.com>
2026-05-07 17:23:00 +0300

    refactored portal and add preauth login for subscribed users

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ define('DEFAULT_RESET_PASSWORD', 'A123a123#');
 return [
     'db' => [
         'host' => 'localhost',
+        'host' => '102.68.86.80',
         'name' => 'hotspot_billing',
         'user' => 'sila',
         'pass' => 'silas1256',
